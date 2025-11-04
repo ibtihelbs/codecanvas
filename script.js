656b3c03db2f53f7a2ec7a131a673e724b31e9f5
ibtihelbs <ibtihel.bensalah@outlook.fr>
2025-11-04 09:26:34 +0100

    rect

--- script.js
+++ script.js
@@ -80,3 +80,31 @@ window.addEventListener("scroll", () => {
     progressBar.style.width = scrolled + "%";
   }
 });
+const countDownDate = new Date("nov 10, 2025 9:00:00").getTime();
+
+// Update the count down every 1 second
+const x = setInterval(function () {
+  // Get today's date and time
+  const now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  const distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("time::after").innerHTML =
+    days + "jours " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
